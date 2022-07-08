fc7199d185c278fb37226f2db77f8f68634d471a
MasonPaddie <masonpaddie29@yahoo.com>
2022-07-07 22:15:22 -0500

    Adds egg

--- js/app.js
+++ js/app.js
@@ -32,3 +32,29 @@ class Tamagotchi {
         this.sleepiness += 1;
     }
 }
+
+const playAppa = () => {
+    document.getElementsByClassName('head1')[0].style.display = 'none'
+    document.getElementsByClassName('head2')[0].style.display = 'none'
+    document.getElementsByClassName('nagaButton')[0].style.display = 'none'
+    document.getElementsByClassName('appaButton')[0].style.display = 'none'
+    document.getElementById('appaImage').style.display = 'none'
+    document.getElementById('nagaImage').style.display = 'none'
+    eggImage = document.getElementById('egg');
+    eggImage.src = "images/egg.png"
+    eggImage.style.position = "absolute"
+    eggImage.style.left = "260px"
+}
+const playNaga = () => {
+    document.getElementsByClassName('head1')[0].style.display = 'none'
+    document.getElementsByClassName('head2')[0].style.display = 'none'
+    document.getElementsByClassName('nagaButton')[0].style.display = 'none'
+    document.getElementsByClassName('appaButton')[0].style.display = 'none'
+    document.getElementById('appaImage').style.display = 'none'
+    document.getElementById('nagaImage').style.display = 'none'
+    eggImage = document.getElementById('egg');
+    eggImage.src = "images/egg.png"
+    eggImage.style.position = "absolute"
+    eggImage.style.right = "260px"
+    eggImage.style.bottom = "0px"
+}
